17ebd691956748c49b03a0bd5835eb298fa61cc2
Brandon M. Flittner <bflittner@bnet.local>
2021-04-06 00:59:43 -0400

    fix

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,5 +1,5 @@
 export class Constants {
-    private static API_URL = "https://tacpoint-controller-001.adaptive-api.com/api/v0/"
+    private static API_URL = "https://tacpoint-master-001.adaptive-api.com/api/v0/"
     // private static API_URL = "http://localhost:4444/v1/api/"
     public static GET_ENDPOINTS = Constants.API_URL + "getEndpoints"
     public static GET_SYSINFO = Constants.API_URL + "sysinfo/"
