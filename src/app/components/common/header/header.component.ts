ce0eb02c5e362d2a634ba30242054f5dec59e0ec
Brandon M. Flittner <bflittner@bnet.local>
2021-05-10 20:23:25 -0400

    mobile

--- src/app/components/common/header/header.component.ts
+++ src/app/components/common/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+@ViewChild('navBar') navBar: ElementRef;
+@ViewChild('navBurger') navBurger: ElementRef;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleNavbar(){
+    this.navBar.nativeElement.classList.toggle('is-active');
+    this.navBurger.nativeElement.classList.toggle('is-active');
   }
 
 }
